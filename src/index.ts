9b585096b20d03968017bd01d57124a55550b7b5
Reimar Rosas <reimarrosas0208@gmail.com>
2021-08-29 05:32:07 -0400

    Added middlewares, port listening and basic get route to the app

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { config } from 'dotenv';
+
+config({
+  path: `${__dirname}/../env/.env`
+});
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}));
+
+app.use(helmet());
+
+const PORT: number = parseInt(process.env['PORT'] as string) || 5000;
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
+
+app.get('/', (_req, res) => {
+  res.status(200).json({
+    message: "Hello, World!"
+  })
+});
